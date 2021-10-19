7d1b9ad0fa965aeea9b27c0e6efac669002e92df
hendradev.py@gmail.com <hendradev.py@gmail.com>
2021-10-19 17:49:18 +0700

    initial commit

---
+++ src/test/java/com/epayment/EpaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
